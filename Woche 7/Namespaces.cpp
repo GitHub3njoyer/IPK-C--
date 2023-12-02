72778a107d980500585d3656305c80dcf6e39fdf
Malte Herzog <malte.herzog.klemenz@gmail.com>
2023-12-02 13:40:13 +0100

    namespaces wurden benutzt in Namespaces.cpp

--- Woche 7/Namespaces.cpp
+++ Woche 7/Namespaces.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    std::pair <std::string, int> name_and_grade = std::make_pair ("Max", 2);
-    std::cout   << "Name: " << name_and_grade.first << std::endl
-                << "Note: " << name_and_grade.second << std::endl;
+    pair <string, int> name_and_grade = make_pair ("Max", 2);
+    cout   << "Name: " << name_and_grade.first << endl
+                << "Note: " << name_and_grade.second << endl;
 }
